032502bd5b7f0d95f214b461f9f14ed2b52270c4
DwarfPorter <mrvlmor@gmail.com>
2020-04-13 21:30:56 +0300

    Кнопка инкрементирует поле

--- lifecycle/app/src/main/java/ru/geekbrains/lifecycle/MainActivity.java
+++ lifecycle/app/src/main/java/ru/geekbrains/lifecycle/MainActivity.java
@@ -1,11 +1,20 @@
 package ru.geekbrains.lifecycle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LIFECYCLE = "LIFE_CYCLE";
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,48 @@ public class MainActivity extends AppCompatActivity {
         else{
             makeToast("Повторный запуск!");
         }
+
+        final EditText etext = findViewById(R.id.editText);
+        final TextView text = findViewById(R.id.textView);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                etext.setText(((Integer)counter).toString());
+                text.setText(((Integer)counter).toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("ON Resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("ON Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("ON Stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("ON restart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("ON Destroy");
     }
 
     @Override
@@ -26,9 +77,20 @@ public class MainActivity extends AppCompatActivity {
         makeToast("ON Start");
     }
 
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        makeToast("Повторный запуск onRestoreInstanceState");
+    }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        makeToast("Сохранение данных onSaveInstanceState");
+    }
 
     private void makeToast(String message){
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d(LIFECYCLE, message);
     }
 }
